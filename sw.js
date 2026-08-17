e7cc1ac732426955722b582707a72da1404dee92
Jan Sotero <148334442+JanSotero@users.noreply.github.com>
2026-08-17 15:24:39 +0200

    Improve fetch event response for navigation requests
    Enhance fetch event handling to return an offline response.

--- sw.js
+++ sw.js
@@ -2,5 +2,11 @@ self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
 
 self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  if (e.request.mode === 'navigate') {
+    e.respondWith(
+      fetch(e.request).catch(() =>
+        caches.match(e.request).then(r => r || new Response('Offline', { status: 503 }))
+      )
+    );
+  }
 });
